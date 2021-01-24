f5eceee57a4971fe18cc4654c00f5be0b971954d
tsilicani <silicani80@gmail.com>
2021-01-24 12:02:02 +0100

    Work in progress.

--- push.sh
+++ push.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 _default_message="Work in progress."
-read -p "Enter the git commit message: [default=\"$_default_message\"] " message
-: ${message:=$_default_message}
-echo "The git commit message will be: $message"
+read -p "Enter the git commit message: [default=\"$_default_message\"] " _message
+: ${_message:=$_default_message}
+echo "The git commit message will be: \"$_message\""
 
 git add .
-git commit -m"${message}"
+git commit -m"${_message}"
 
 if git diff-index --quiet HEAD --; then
     echo "Changes detected."
-    git push -u origin master
+    git push
 else
     echo "No Changes detected: nothing to push."
 fi
