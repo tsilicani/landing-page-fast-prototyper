7028b3a8c619e91405e87460ee10e71e49277c19
tsilicani <silicani80@gmail.com>
2021-01-24 13:52:44 +0100

    Work in progress

--- push.sh
+++ push.sh
@@ -8,7 +8,7 @@ git add .
 git commit -m"${_message}"
 git push
 
-echo '                               
+echo -e '                               
                |             ||
 ,---..   .,---.|---.,---.,---||
 |   ||   |`---.|   ||---'|   | 
